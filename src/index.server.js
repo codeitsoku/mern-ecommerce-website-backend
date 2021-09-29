01ab34401f084325d33a98adb21eefc0bce526ec
Souvik <csouvik21@gmail.com>
2021-09-29 21:13:27 +0530

    Initial Commit

---
+++ src/index.server.js
@@ -0,0 +1,44 @@
+const express=require('express');
+const env=require('dotenv');
+const app=express();
+const mongoose=require('mongoose');
+const path=require('path');
+const cors =require('cors');
+
+//routes
+const authRoutes=require('./routes/auth');
+const adminRoutes=require('./routes/admin/auth');
+const categoryRoutes = require('./routes/category');
+const productRoutes = require('./routes/product');
+const cartRoutes = require('./routes/cart');
+
+//environment variable or constants
+env.config();
+
+//mongodb Connection
+//mongodb+srv://root:<password>@cluster0.eaiun.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+mongoose.connect(
+    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.eaiun.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`, 
+    {
+        useNewUrlParser: true, 
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    }
+).then(() => {
+    console.log('Database Connected');
+});
+
+
+app.use(cors());
+app.use(express.json());
+app.use('/public',express.static(path.join(__dirname, 'uploads')));
+app.use('/api', authRoutes);
+app.use('/api', adminRoutes);
+app.use('/api', categoryRoutes);
+app.use('/api', productRoutes);
+app.use('/api', cartRoutes);
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
